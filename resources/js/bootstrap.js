cedd26a75049494ab949acc4b7d3b3acc3e1a826
JesusGarciaValadez <jesus.garciav@me.com>
2025-02-27 12:00:52 -0600

    feat: install Laravel Sanctum

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,8 @@
+"use strict"
+
+import axios from 'axios';
+window.axios = axios;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
